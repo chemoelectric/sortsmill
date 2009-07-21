dde61baf3665d77f06683f5d4298b69cd7f2620c
Barry Schwartz <chemoelectric@chemoelectric.org>
2009-07-20 22:06:38 -0500

    Initial commit of Valley build script.

---
+++ valley/Valley_build.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2009 Barry Schwartz
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import fontforge
+import spacing_by_anchors
+
+
+def build_glyphs(bitbucket, f):
+
+    if f.persistent == None:
+        f.persistent = {}
+    f.persistent['spacing_anchor_heights'] = { 'hi' : 1350,
+                                               'x'  : 852,
+                                               'o'  : 453,
+                                               'bl' : 40,
+                                               'lo' : -420 }
+
+    propagate_hyphens(f)
+
+    make_glyph_reference("quotesingle", f["quoteright"])
+    make_glyph_reference("quotedbl", f["quotedblright"])
+
+    f.selection.all()
+    spacing_by_anchors.space_selected_by_anchors(f)
+    f.selection.none()
+
+    generate_kerning_and_read_features(None, f)
