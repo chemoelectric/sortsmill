dc513dc34eb007bf0672c4333e333e7eb5ce5326
Barry Schwartz <chemoelectric@chemoelectric.org>
2009-10-10 00:57:16 -0500

    Improvement to TrueType generation of Linden Hill: replace with references where possible.

--- lindenhill/make-fonts.py
+++ lindenhill/make-fonts.py
@@ -86,6 +86,9 @@ for sfd_path in sys.argv[1:]:
     f.selection.all()
     f.canonicalStart()
     f.canonicalContours()
+    if tt_match:
+        f.replaceWithReference()
+        f.correctReferences()
     f.selection.none()
 
     if tt_match:
