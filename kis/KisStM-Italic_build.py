e0d00127a927de6d30866214fec29ac867f55f34
Barry Schwartz <chemoelectric@chemoelectric.org>
2010-03-05 17:24:38 -0600

    FontForge ate my homework and I had to revert. Therefore I am checking in Kis Italic with anchors for making dotted i and j so I won't accidentally lose my i and j outlines if FF eats my homework again.

--- kis/KisStM-Italic_build.py
+++ kis/KisStM-Italic_build.py
@@ -242,10 +242,10 @@ def build_glyphs(bitbucket, f):
 #        build_accented_glyph(letter + 'dotaccent.sc', f[letter + '.sc'], f['uni0307'])
 #    build_accented_glyph('i.TRK.sc', f['i.sc'], f['uni0307'])
 
-#    build_accented_glyph('i', f['dotlessi'], f['uni0307'])
-#    build_accented_glyph('iogonek', f['iogonek.dotless'], f['uni0307'])
-#    build_accented_glyph('j', f['uni0237'], f['uni0307'])
-#    build_accented_glyph('j.ld', f['uni0237.ld'], f['uni0307'])
+    build_accented_glyph('i', f['dotlessi'], f['uni0307'])
+    build_accented_glyph('iogonek', f['iogonek.dotless'], f['uni0307'])
+    build_accented_glyph('j', f['uni0237'], f['uni0307'])
+    build_accented_glyph('j.ld', f['uni0237.ld'], f['uni0307'])
 
     #--------------------------------------------------------------------------
 
