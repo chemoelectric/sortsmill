f976946221b361e218e052a45b1d251f447405d2
Barry Schwartz <github@crudfactory.com>
2011-04-13 22:49:45 -0500

    asy.py: Glyph-view tool to print out OCaml code for contours.

--- tools/asy.py
+++ tools/asy.py
@@ -3,7 +3,13 @@
 """
 A module for use with Asymptote.
 See http://asymptote.sourceforge.net/
+(But Asymptote support is likely to be
+dropped or reduced.)
 
+For now also a module for use with OCaml, a programming language
+favored at the Sorts Mill. See http://caml.inria.fr/
+And some of it might be used with any programming or scripting
+language your prefer.
 
 
 Copyright (c) 2011 Barry Schwartz
@@ -27,6 +33,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
 
+import cmath
 import fontforge
 import imp
 import subprocess
@@ -52,6 +59,141 @@ def asymptote_path(contour):
             s += '--\n'
     if contour.closed:
         s += 'cycle'
+    return s
+
+#--------------------------------------------------------------------------
+
+direction_tolerance = 1e-4
+linear_tolerance = 1e-4
+decimal_places = 4
+
+def move_to_first_quadrant(c):
+    return complex(abs(c.real), abs(c.imag))
+
+def caml_complex(c):
+    x = c.real
+    y = c.imag
+
+    if x < 0:
+        s_x = ("x'({0:." + str(decimal_places) + "f})").format(x)
+    elif x == 0:
+        s_x = None
+    else:
+        s_x = ("x' {0:." + str(decimal_places) + "f}").format(x)
+
+    if y < 0:
+        s_y = ("y'({0:." + str(decimal_places) + "f})").format(y)
+    elif y == 0:
+        s_y = None
+    else:
+        s_y = ("y' {0:." + str(decimal_places) + "f}").format(y)
+
+    if s_x is None and s_y is None:
+        s = "zero"
+    elif s_x is None:
+        s = s_y
+    elif s_y is None:
+        s = s_x
+    else:
+        s = s_x + " + " + s_y
+
+    return s
+
+def caml_path(contour):
+
+    "Returns OCaml code for a contour, in the notations of the Sorts Mill's Fontdesign module."
+
+    plist = list(contour)
+    if 3 <= len(plist) and not plist[-2].on_curve and not plist[-1].on_curve:
+        plist = plist[-1:] + plist[:-1]
+
+    plist2 = []
+    i = 0
+    while i < len(plist):
+        if plist[i].on_curve:
+            if i + 1 < len(plist) and not plist[i + 1].on_curve:
+                plist2 += [plist[i], plist[i], plist[i + 1]]
+                i += 2
+            else:
+                plist2 += [plist[i], plist[i], plist[i]]
+                i += 1
+        elif i + 2 < len(plist) and not plist[i + 2].on_curve:
+            plist2 += [plist[i], plist[i + 1], plist[i + 2]]
+            i += 3
+        else:
+            plist2 += [plist[i], plist[i + 1], plist[i + 1]]
+            i += 2
+
+    s = "of_node_list [\n"
+    i = 0
+    while i < len(plist2):
+        inhandle_x = plist2[i].x - plist2[i + 1].x
+        inhandle_y = plist2[i].y - plist2[i + 1].y
+        oncurve_x = plist2[i + 1].x
+        oncurve_y = plist2[i + 1].y
+        outhandle_x = plist2[i + 2].x - plist2[i + 1].x
+        outhandle_y = plist2[i + 2].y - plist2[i + 1].y
+
+        c_inhandle = complex(inhandle_x, inhandle_y)
+        c_oncurve = complex(oncurve_x, oncurve_y)
+        c_outhandle = complex(outhandle_x, outhandle_y)
+
+        a1 = abs(c_inhandle)
+        a2 = abs(c_outhandle)
+        d1 = c_inhandle / a1
+        d2 = c_outhandle / a2
+        df1 = move_to_first_quadrant(d1)
+        df2 = move_to_first_quadrant(d2)
+
+        is_flat = (linear_tolerance <= a1 and
+                   linear_tolerance <= a2 and
+                   abs(d1 + d2) < direction_tolerance)
+        is_horizontal = (is_flat and
+                         abs(df1 - 1) < direction_tolerance and
+                         abs(df2 - 1) < direction_tolerance)
+        is_vertical = (is_flat and
+                       not is_horizontal and
+                       abs(df1 - complex(0,1)) < direction_tolerance and
+                       abs(df2 - complex(0,1)) < direction_tolerance)
+        is_right = is_horizontal and abs(d1 + 1) < direction_tolerance
+        is_left = is_horizontal and not is_right
+        is_up = is_vertical and abs(d1 + complex(0,1)) < direction_tolerance
+        is_down = is_vertical and not is_up
+
+        if is_right:
+            s += ("  make_right (" +
+                  caml_complex(c_oncurve) + ") (" +
+                  caml_complex(a1) + ") (" +
+                  caml_complex(a2) + ");\n")
+        elif is_left:
+            s += ("  make_left (" +
+                  caml_complex(c_oncurve) + ") (" +
+                  caml_complex(a1) + ") (" +
+                  caml_complex(a2) + ");\n")
+        elif is_up:
+            s += ("  make_up (" +
+                  caml_complex(c_oncurve) + ") (" +
+                  caml_complex(a1) + ") (" +
+                  caml_complex(a2) + ");\n")
+        elif is_down:
+            s += ("  make_down (" +
+                  caml_complex(c_oncurve) + ") (" +
+                  caml_complex(a1) + ") (" +
+                  caml_complex(a2) + ");\n")
+        elif is_flat:
+            s += ("  make_flat (" +
+                  caml_complex(c_oncurve) + ") (" +
+                  caml_complex(d2) + ") (" +
+                  caml_complex(a1) + ") (" +
+                  caml_complex(a2) + ");\n")
+        else:
+            s += ("  make_node (" +
+                  caml_complex(c_inhandle) + ") (" +
+                  caml_complex(c_oncurve) + ") (" +
+                  caml_complex(c_outhandle) + ");\n")
+        i += 3
+    s += "] <@@ " + ("true" if contour.closed else "false")
+
     return s
 
 #--------------------------------------------------------------------------
@@ -95,5 +237,15 @@ def print_asymptote_paths(glyph):
 fontforge.registerMenuItem((lambda _, glyph: print_asymptote_paths(glyph)),
                            None, None, 'Glyph', 'None',
                            'Output Asymptote paths')
+
+#--------------------------------------------------------------------------
+
+def print_caml_contours(glyph):
+    for c in glyph.layers[glyph.activeLayer]:
+        print(caml_path(c))
+
+fontforge.registerMenuItem((lambda _, glyph: print_caml_contours(glyph)),
+                           None, None, 'Glyph', 'None',
+                           'Output OCaml contours')
 
 #--------------------------------------------------------------------------
